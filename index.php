50516a0c9e7d229ac75361412f384d4702973d1b
Szymon Machajewski <support@dataii.com>
2020-11-16 22:36:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?PHP
 
-$room = $_POST['context_title']." Room(".substr(hash("sha512", $_POST['context_title']),0,9).")";
+$room = $_POST['context_title']." Room(".substr(hash("sha512", $_POST['context_title'].$_POST['context_label']),0,9).")";
 header("location: https://meet.jit.si/".$room);
 
 ?>
